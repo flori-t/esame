8f746c2ef743abd8ef166ec6270d235e80a5df59
Your Name <you@example.com>
2026-02-05 14:21:35 +0100

    Aggiunta di 'greet.php' al repository remoto

---
+++ greet.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Ritorna un saluto personalizzato.
+ */
+function greet(string $name): string
+{
+    return "Ciao, {$name}!";
+}
+
+// Comando di esecuzione: php hello.php [nome]
+// Esempio: php hello.php Mario
+$name = $argv[1] ?? "Mondo";
+echo greet($name) . PHP_EOL;
